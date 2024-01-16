b38cbc5612df4c5be6b4dade766940d23fed098a
vsymnyivs <156124819+vsymnyivs@users.noreply.github.com>
2024-01-16 03:06:05 +0300

    Update str_func.py
    изменил докстринг

--- str_func.py
+++ str_func.py
@@ -1,6 +1,7 @@
 def upper_func(string):
     """
     Функция принимает ввод и возвращает текст с заглавными буквами
+    выполнил изменение докстринга
     """
     string_upper = string.upper()
     return string_upper
